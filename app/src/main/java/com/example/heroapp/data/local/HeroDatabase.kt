f03b3e1216b41737c0abca63d350fc04d2c1b449
Niranjan V <rathinamniranjan@gmail.com>
2024-03-21 23:05:19 +0530

    Dagger Hilt for Room Database

--- app/src/main/java/com/example/heroapp/data/local/HeroDatabase.kt
+++ app/src/main/java/com/example/heroapp/data/local/HeroDatabase.kt
@@ -1,6 +1,7 @@
 package com.example.heroapp.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.heroapp.data.local.dao.HeroDao
 import com.example.heroapp.domain.model.Hero
@@ -8,7 +9,7 @@ import com.example.heroapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class,HeroRemoteKey::class], version = 1)
 @TypeConverters(DatabaseConverter::class)
-abstract class HeroDatabase {
+abstract class HeroDatabase :RoomDatabase(){
 
     abstract fun getDao():HeroDao
     abstract fun getRemoteKeyDao():HeroRemoteKey
--- app/src/main/java/com/example/heroapp/util/Constants.kt
+++ app/src/main/java/com/example/heroapp/util/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val DETAILS_SCREEN_ARGS = "heroId"
     const val HERO_TABLE_NAME = "hero_table"
     const val HERO_REMOTE_KEY_TABLE = "hero_remote_key_table"
+    const val HERO_DATABASE = "hero_db"
 
 }
